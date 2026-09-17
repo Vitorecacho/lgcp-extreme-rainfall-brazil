7c9c8e27e7a02309addddb3e1957a186f32dcf01
Vitor Recacho <93835880+Vitorecacho@users.noreply.github.com>
2026-09-17 15:02:44 -0300

    Update year range from 2003-2026 to 1981-2026

--- scripts/baixachirps.sh
+++ scripts/baixachirps.sh
@@ -2,7 +2,7 @@
 
 BASE_URL="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p05"
 
-for year in $(seq 2003 2026); do
+for year in $(seq 1981 2026); do
   FILE="chirps-v2.0.${year}.days_p05.nc"
   echo "Downloading $FILE..."
   wget -c "${BASE_URL}/${FILE}"
